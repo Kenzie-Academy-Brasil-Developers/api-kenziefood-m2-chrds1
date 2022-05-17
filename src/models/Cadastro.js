b333a492717813de6bdfded6eef3710f18e37eaa
lorenammp <lorenammpaula@gmail.com>
2022-05-17 12:51:57 -0300

    Resolvidos detalhes que causavam alguns erros no cadastro

--- src/models/Cadastro.js
+++ src/models/Cadastro.js
@@ -1,31 +1,32 @@
-import Api from "../controllers/Api"
+import Api from "../controllers/Api.js"
 
-const formularioCadastro = document.getElementById("fomr-cadastro")
-
-formularioCadastro.addEventListener("submit", cadastro.cadastrarUsuario)
-
-class cadastro{
-
+class Cadastro{
     static cadastrarUsuario(event){
-        event.preventDefault()
+        event.preventDefault();
         
-        const dadosUsuario = this.capiturarDadosFormularios(event)
-        console.log(dadosUsuario)
-       Api.criarUsuario(dadosUsuario)
+        const dadosUsuario = this.capiturarDadosFormularios(event);
+        console.log(dadosUsuario);
+        Api.criarUsuario(dadosUsuario);
 
     }
-
-    static capiturarDadosFormularios(event){
-        event.preventDefault()
+    
+    static capiturarDadosFormularios(event) {
+        event.preventDefault();
         const dadosForm = [... event.currentTarget];
         const dadosUsuario = {};
     
         dadosForm.forEach((dado)=>{
             if(dado.name != "" && dado.name != "cidade"){
-                dadosUsuario[dado.name] = dado.value
+                dadosUsuario[dado.name] = dado.value;
             }
         })
+
+        console.log(dadosUsuario);
     
-        return dadosUsuario
+        return dadosUsuario;
     }
 }
+
+const formularioCadastro = document.getElementById("form-cadastro");
+
+formularioCadastro.addEventListener("submit", Cadastro.cadastrarUsuario);
