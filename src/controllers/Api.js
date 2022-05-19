06d35d05a52b94928a697aa9f4ce83ba0e127151
lorenammp <lorenammpaula@gmail.com>
2022-05-19 11:51:56 -0300

    Criada as requisições para criar produtos, editar e deletar através da dashboard do usuário

--- src/controllers/Api.js
+++ src/controllers/Api.js
@@ -60,6 +60,65 @@ class Api {
 
         return response;
     }
+
+    static async produtosPrivados() {
+        const response = await fetch('https://api-kenzie-food.herokuapp.com/my/products', {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        .then((res) => res.json())
+        .then((res) => res)
+        .catch((error) => error);
+
+        return response;
+    }
+
+    static async criarProduto(produto) {
+        const response = await fetch('https://api-kenzie-food.herokuapp.com/my/products', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(produto)
+        })
+        .then((res) => JSON.stringify(produto))
+        .then((res) => res)
+        .catch((error) => error);
+
+        return response;
+    }
+
+    static async editarProduto(id, produto) {
+        const response = await fetch('https://api-kenzie-food.herokuapp.com/my/products/'+id, {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${localStorage.getItem('Token')}`
+            },
+            body: JSON.stringify(produto)
+        })
+        .then((res) => res.json())
+        .then((res) => res)
+        .catch((error) => error);
+
+        return response;
+    }
+
+    static async deletarProduto(id) {
+        const response = await fetch('https://api-kenzie-food.herokuapp.com/my/products/'+id, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${localStorage.getItem('Token')}`
+            }
+        })
+        .then((res) => res)
+        .catch((error) => error);
+
+        return response;
+    }
 }
 
 export default Api
